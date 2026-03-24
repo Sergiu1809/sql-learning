0c93e422f4dd8c88fd38bcdd2dc872b82046753d
Tanase Sergiu <tanasesergiu15@gmail.com>
2026-03-24 22:45:38 +0200

    Add SQL language overview and create users table

--- queries.sql
+++ queries.sql
@@ -1,11 +1,10 @@
--- SQL - The language of Databases
--- SQL (Structured Query Language) is the language you use to talk to a database.
--- Every time you want to store, read, update or delete data you write SQL
--- It's not a programming language like Python - it's a query language.
--- You describe what you want, not how to get it.
--- Database -> Excel but more powerful
--- Excel:
--- | Name    | Age | City    |
--- |---------|-----|---------|
--- | Sergiu  | 20  | Sibiu   |
--- | Alex    | 25  | Cluj    |
+-- Creating tables
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, -- auto-incrementing number, unique identifier
+    name VARCHAR(100) NOT NULL, -- text up to 100 characters & required, can't be null
+    email VARCHAR(100) UNIQUE NOT NULL, -- no two users can have the same email
+    age INTEGER, -- whole number, optional
+    city VARCHAR(50) -- optional
+)
+
